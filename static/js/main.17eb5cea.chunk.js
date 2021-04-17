86cbfb8b1f11621dce0e8fbc5162cb32a591713d
Roshni <rnp9592@gmail.com>
2021-04-17 14:42:21 -0400

    Updates

---
+++ static/js/main.17eb5cea.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdynamic-inline-style-practice"]=this["webpackJsonpdynamic-inline-style-practice"]||[]).push([[0],{4:function(e,o,n){"use strict";n.r(o);n(1);var c,t=n(3),r=n.n(t),i=n(0),l=(new Date).getHours(),s={color:" "};l<12?(c="Good Morning",s.color="red"):l>=12&&l<=17?(c="Good Afternoon",s.color="green"):l>=17&&l<=24&&(c="Good Night",s.color="blue"),r.a.render(Object(i.jsx)("div",{children:Object(i.jsxs)("h1",{className:"heading",style:s,children:["Hello! ",c]})}),document.getElementById("root"))}},[[4,1,2]]]);
+//# sourceMappingURL=main.17eb5cea.chunk.js.map
